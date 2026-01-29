c6ea9a1d43ce8c483ac38682763b7139a558d112
Antoni Balcerzak <antekbal06@gmail.com>
2026-01-29 22:20:37 +0100

    Implemented tests for vehicle validator

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app.services.pricing import PriceCalculator
+from app.services.validator import VehicleValidator
 
 @pytest.fixture
 def parking_prices():
@@ -8,3 +9,15 @@ def parking_prices():
 @pytest.fixture
 def price_calculator(parking_prices):
     return PriceCalculator(parking_prices)
+
+@pytest.fixture
+def basic_letters():
+    return ["B", "C", "D", "E", "F", "G", "K", "L", "N", "O", "P", "R", "S", "T", "W", "Z"]
+
+@pytest.fixture
+def special_letters():
+    return ["H", "U"]
+
+@pytest.fixture
+def vehicle_validator(basic_letters, special_letters):
+    return VehicleValidator(basic_letters, special_letters)
